58b6499cffa8015270ed6e355c852fe263c61779
viscoci <ethan.mccosby@gmail.com>
2020-04-17 16:21:37 -0400

    Experimenting with automatic updates to postman collections through the postman api

---
+++ src/interfaces/PostmanEnvironment.ts
@@ -0,0 +1,19 @@
+/** ISO Date as a string */
+export type ISOdate = string;
+
+export interface PostmanEnvironmentValue
+{
+    key: string;
+    value: string;
+    enabled: boolean;
+}
+
+export interface PostmanEnvironment
+{
+    id: string;
+    name: string;
+    values: PostmanEnvironmentValue[];
+    _postman_variable_scope: "environment";
+    _postman_exported_at: ISOdate;
+    _postman_exported_using: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,5 @@ export * from './DecoratorData';
 export * from './Metadata';
 export * from './ExportOptions';
 export * from './PostmanTest';
+export * from './PostmanAuth';
+export * from './PostmanEnvironment';
